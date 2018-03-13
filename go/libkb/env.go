0be9fa4f0cfe280392c8ab7a11d728f0729b8ae2
Michał Zochniak <michal@zapu.net>
2018-03-13 11:15:19 +0100

    Option to override ptrace blocking with KEYBASE_ALLOW_PTRACE env var

--- go/libkb/env.go
+++ go/libkb/env.go
@@ -1343,3 +1343,9 @@ func GetPlatformString() string {
 	}
 	return runtime.GOOS
 }
+
+func (e *Env) AllowPTrace() bool {
+	return e.GetBool(false,
+		func() (bool, bool) { return e.getEnvBool("KEYBASE_ALLOW_PTRACE") },
+	)
+}
